65c4f02d558d8411ee4b53f4c4571284c6e70e5c
Ghenet Andemiachael <ghenikibrom@gmail.com>
2021-02-22 13:37:00 -0800

    changes the “status” text in Board’s render so that it displays which player has the next turn

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ function Square(props) {
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player:' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
