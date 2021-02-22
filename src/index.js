6768f3d3232be8129fb451f405606b3e8ec5b762
Ghenet Andemiachael <ghenikibrom@gmail.com>
2021-02-22 12:47:10 -0800

    Square call that function when a square is clicked

--- src/index.js
+++ src/index.js
@@ -32,7 +32,10 @@ class Square extends React.Component {
     }
 
     renderSquare(i) {
-      return <Square value={this.state.squares[i]} />;
+      return <Square 
+                value={this.state.squares[i]} 
+                onClick={() => this.handleClick(i)}
+                />;
     }
   
     render() {
